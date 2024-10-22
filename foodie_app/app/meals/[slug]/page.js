0de565c5b4d17cc0d474bf072276b66b7f24b2d2
= <=>
2024-10-22 10:15:33 -0400

    Route setup

---
+++ foodie_app/app/meals/[slug]/page.js
@@ -0,0 +1,5 @@
+const MealSlugPage = ({ params }) => {
+    return <h1>Meals Slug: {params.slug}</h1>
+}
+
+export default MealSlugPage
---
+++ foodie_app/app/meals/share/page.js
@@ -0,0 +1,5 @@
+const MealSharePage = () => {
+    return <h1>Meal Share Page</h1>
+}
+
+export default MealSharePage;
